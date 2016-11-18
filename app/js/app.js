c8a10c50cf4b82c8682100605ce50b1c245728b4
monkeysbrain <monkeysbrain@yandex.ru>
2016-11-18 22:36:50 +0500

    don't understand for gallery (slider, gallery, or what)

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,10 @@
 $(function(){
+	//top-menu
 	$('.menu-header').click(function () {
         $('ul').toggleClass("open")
-    })
+    });
+
+    $('.gallery-list--item').click(function(){
+		$(".gallery-current img").attr("src", $(this).attr("src"));
+	});
 });
